979917c20e5a8a21a98c23a24f2de526b5986834
aioverg <aioverg@outlook.com>
2024-11-17 12:17:11 +0800

    init

--- scripts/service_worker.js
+++ scripts/service_worker.js
@@ -1,14 +1,39 @@
-let rule1 = {
-  conditions: [
-    new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { hostEquals: '127.0.0.1' },
-    }),
-  ],
-  actions: [new chrome.declarativeContent.ShowPageAction()],
-};
 
-chrome.declarativeContent.onPageChanged.removeRules(undefined, data => {
-  chrome.declarativeContent.onPageChanged.addRules([rule1], data => {
-    console.log('addRules', data);
+// 加载图片
+async function loadImageData(url) {
+  const img = await createImageBitmap(await (await fetch(url)).blob())
+  const { width: w, height: h } = img; const canvas = new OffscreenCanvas(w, h)
+  const ctx = canvas.getContext('2d')
+  ctx.drawImage(img, 0, 0, w, h)
+  return ctx.getImageData(0, 0, w, h)
+}
+
+chrome.runtime.onInstalled.addListener(async () => {
+
+  chrome.action.disable(); // 禁用插件
+
+  // 载入图片
+  const png16 = await loadImageData("/icons/16.png")
+
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    // 启用插件规则
+    let enableRule = {
+      id: 'enableRule',
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostSuffix: '.baidu.com', schemes: ['https'] },
+        })
+      ],
+      actions: [
+        new chrome.declarativeContent.ShowAction(), // 在当前条件下启用插件
+        new chrome.declarativeContent.SetIcon({ imageData: { 16: png16 } }), // 将图标设置为彩色, manifest.js 中默认图标为灰色
+      ],
+    }
+    let rules = [enableRule];
+    chrome.declarativeContent.onPageChanged.addRules(rules);
   });
 });
+
+chrome.tabs.onActivated.addListener((calb) => {
+  chrome.action.setBadgeText({ text: 'ON' }); // 增加文字
+})
